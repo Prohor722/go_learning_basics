3cf3049f3a8bcd052bdc072f4bc7d4b26867f0e3
Prohor722 <prohor.b722@gmail.com>
2025-12-02 04:38:51 +0600

    bug fixed

--- first_program.go
+++ first_program.go
@@ -4,6 +4,8 @@ func main() {
 	print("Prohor ")
 	println("Banik")
 
+	println("print \\ slash")
+	println("print \"double quation\".")
 	println("line \nbreak") // \n will not create a new line here
 	println("add tab \t here") // \t will not create a tab here
 }
