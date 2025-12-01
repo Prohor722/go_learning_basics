af685e78c7623e18f1709f592e4b8d3fa3402dc0
Prohor722 <prohor.b722@gmail.com>
2025-12-02 04:32:23 +0600

    tab example

--- first_program.go
+++ first_program.go
@@ -4,6 +4,6 @@ func main() {
 	print("Prohor ")
 	println("Banik")
 
-	println("line \n break") // \n will not create a new line here
-	
+	println("line \nbreak") // \n will not create a new line here
+	println(`add tab \t here`) // \t will not create a tab here
 }
