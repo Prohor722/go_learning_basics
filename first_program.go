2d88db6555f253fdedaf1a677f1155c6a9a18315
Prohor722 <prohor.b722@gmail.com>
2025-12-02 04:35:49 +0600

    bug fixed

--- first_program.go
+++ first_program.go
@@ -5,5 +5,5 @@ func main() {
 	println("Banik")
 
 	println("line \nbreak") // \n will not create a new line here
-	println(`add tab \t here`) // \t will not create a tab here
+	println("add tab \t here") // \t will not create a tab here
 }
