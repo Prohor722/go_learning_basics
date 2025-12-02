db53d006526f9d72ae54257ec022acc7cda00108
Prohor722 <prohor.b722@gmail.com>
2025-12-02 17:51:40 +0600

    float added

--- variables.go
+++ variables.go
@@ -1,10 +1,17 @@
 package main
 
+import "fmt"
+
 func showVariables() {
 	a:= 10
 	b:= 20
+	d:= 30.5
+	e:= 19.5333
+	// _ = d // to avoid unused variable error
 	str:= "Hello, Go!"
 	sum := a + b
+	sum2 := d + e
 	println("Sum is:", sum)
+	fmt.Println("Sum of float is:", sum2)	// using fmt package to print float sum
 	println("String is:", str)
 }
