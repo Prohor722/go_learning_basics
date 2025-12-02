b9dd2bc8dbdd9f406347a4692872a2cd7bb28aaa
Prohor722 <prohor.b722@gmail.com>
2025-12-02 20:48:47 +0600

    printf

--- variables.go
+++ variables.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func showVariables() {
 	a:= 10
 	var b int = 20
 	d:= 30.5
 	var e float64 = 19.5333
+	var f float32 = 12.33
+	var g float32 = 45.66
+
+	var sum3 float32 = f + g
 	// _ = d // to avoid unused variable error
 	str:= "Hello, Go!"
 	var str2 string = "Go programming."
@@ -14,6 +20,10 @@ func showVariables() {
 	sum2 := d + e
 	println("Sum is:", sum)
 	fmt.Println("Sum of float is:", sum2)	// using fmt package to print float sum
+	fmt.Println("Sum of float32 is:", sum3)
 	println("String is:", str)
 	println(str2)
+
+	println("this is a variable: ",a)
+	fmt.Printf("this is a variable: %d\n", b)
 }
