d367cdca4a376b94bf7333e706e4acb1a00e3bec
Prohor722 <prohor.b722@gmail.com>
2025-12-02 20:54:42 +0600

    const exmple added

--- variables.go
+++ variables.go
@@ -11,6 +11,9 @@ func showVariables() {
 	var e float64 = 19.5333
 	var f float32 = 12.33
 	var g float32 = 45.66
+	const abc = 100
+	const pi float64 = 3.14159
+	const str1 string = "Constant String"
 
 	var sum3 float32 = f + g
 	// _ = d // to avoid unused variable error
@@ -22,9 +25,11 @@ func showVariables() {
 	fmt.Println("Sum of float is:", sum2)	// using fmt package to print float sum
 	fmt.Println("Sum of float32 is:", sum3)
 	println("String is:", str)
-	fmt.Printf("One line String is: %v", str)
+	fmt.Printf("One line String is: %v\n", str)
 	println(str2)
 
 	println("this is a variable: ",a)
 	fmt.Printf("this is a variable: %d\n", b)
+
+	fmt.Printf("Constant abc: %d, pi: %.5f, str1: %s\n", abc, pi, str1)
 }
