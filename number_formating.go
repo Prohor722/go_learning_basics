746a8e6b4cc757100be9703892e52a96e0d7a3cd
Prohor722 <prohor.b722@gmail.com>
2025-12-03 23:43:11 +0600

    number formating exmple added

--- number_formating.go
+++ number_formating.go
@@ -10,5 +10,11 @@ func formatVariables() {
 	print("Enter a decimal number: ")
 	fmt.Scan(&decimalNumber)
 
-	fmt.Printf("\n You have entered: %b", decimalNumber) // binary format
+	fmt.Printf("\n Your number in binary: %b", decimalNumber) // binary format
+	fmt.Printf("\n You number in decimal: %d", decimalNumber) // decimal format
+	fmt.Printf("\n Your number in hexadecimal: %x\n", decimalNumber) // hexadecimal format
+	fmt.Printf("Your number in octal: %o\n", decimalNumber) // octal format
+	fmt.Printf("Your number with leading zeros: %08d\n", decimalNumber) // leading zeros
+	fmt.Printf("Your number with sign: %+d\n", decimalNumber) // with sign
+	
 }
