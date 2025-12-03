94ce4b8451fca10e3be90835fa920baf944a889e
Prohor722 <prohor.b722@gmail.com>
2025-12-03 23:43:46 +0600

    number formating exmple added

--- number_formating.go
+++ number_formating.go
@@ -16,5 +16,8 @@ func formatVariables() {
 	fmt.Printf("Your number in octal: %o\n", decimalNumber) // octal format
 	fmt.Printf("Your number with leading zeros: %08d\n", decimalNumber) // leading zeros
 	fmt.Printf("Your number with sign: %+d\n", decimalNumber) // with sign
+	fmt.Printf("Your number in width 10: %10d\n", decimalNumber) // width 10
+	fmt.Printf("Your number left aligned in width 10: %-10d\n", decimalNumber) // left aligned width 10
+	fmt.Printf("Your number with comma as thousand separator: %d\n", decimalNumber) // comma as thousand separator
 	
 }
