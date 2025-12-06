c0612f9e0bd49af0a414c0386450ced5a393e770
Prohor722 <prohor.b722@gmail.com>
2025-12-06 23:49:28 +0600

    calculator

--- calculator.go
+++ calculator.go
@@ -6,6 +6,14 @@ import (
 
 func calculator(){
 	var x,y int
+	operator:=""
 	print("Enter two numbers: ")
 	fmt.Scan(&x,&y)
+	
+	print("Enter operator: ")
+	fmt.Scan(&operator)
+
+	switch operator {
+
+
 }
