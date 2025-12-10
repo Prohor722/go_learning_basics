fb2b84103e3208db9118840b01f34807b96d576b
Prohor722 <prohor.b722@gmail.com>
2025-12-10 23:59:17 +0600

    assign operator

--- assign_operator.go
+++ assign_operator.go
@@ -4,6 +4,7 @@ func assignOperator() {
 	x := 10
 	println("when, x := 10 ",x)
 	x += 5	  // x = x + 5
+	println("after x += 5 ",x)
 	x -= 3   // x = x - 3
 	x *= 2   // x = x * 2
 	x /= 4   // x = x / 4
