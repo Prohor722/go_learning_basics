0befac9e0ad191f33b5e4606a57fbf337d9df242
Prohor722 <prohor.b722@gmail.com>
2025-12-13 17:26:15 +0600

    conditions and loops

--- conditionsAndLoops.go
+++ conditionsAndLoops.go
@@ -3,11 +3,14 @@ package main
 func conditions(){
 	a := 10
 	b := 20
-	b -= 15
+	b -= 10
+	a += 10
 
 	if a < b {
 		println("a is less than b")
 	}else if a == b {
 		println("a is equal to b")
+	}else {
+		println("a is greater than b")
 	}
 }
