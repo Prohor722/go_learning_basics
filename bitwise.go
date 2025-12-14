0f388b5d7b205a95fdb64dcc9b7615d47adb0a74
Prohor722 <prohor.b722@gmail.com>
2025-12-14 23:58:26 +0600

    bitwise

--- bitwise.go
+++ bitwise.go
@@ -1,6 +1,11 @@
 package main
 
+import "fmt"
+
 func bitwise(){
 	a := 60        // 60 = 0011 1100
-	b := 13
+	b := 13 	  // 13 = 0000 1101
+
+	fmt.Printf("a & b = %d\n", a & b)   // 12 = 0000 1100
+	fmt.Printf("a | b = %d\n", a | b)   // 61 = 0011 1101
 }
