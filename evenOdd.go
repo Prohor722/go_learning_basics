cf141d58b260533d44a85fd3a7194095f01d4995
Prohor722 <prohor.b722@gmail.com>
2025-12-15 19:04:15 +0600

    even odd

--- evenOdd.go
+++ evenOdd.go
@@ -1,7 +1,12 @@
 package main
 
+import "fmt"
+
 func evenOdd(){
-	num := 7
+	var num int
+	print("Enter an integer: ")
+	fmt.Scanf("%d", &num)
+
 	if num%2 == 0 {
 		println(num, "is even")
 	} else {
