fecca3f1c976a5ffbd9e771afd092a4e314095c6
Prohor722 <prohor.b722@gmail.com>
2025-12-16 17:47:41 +0600

    largest number

--- largestNumber.go
+++ largestNumber.go
@@ -1,4 +1,10 @@
 package main
 
-func largestNumber(a int, b int) int {
+func getNumber(a int, b int){
+	print("Enter the first number:")
+	scanf("%d", &a)
+	print("Enter the second number:")
+	scanf("%d", &b)
+}
+func largestNumber(a int, b int){
 }
