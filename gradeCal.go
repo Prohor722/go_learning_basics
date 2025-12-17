9e48ac4234734fe1ac6c94b2e32a56db50daace6
Prohor722 <prohor.b722@gmail.com>
2025-12-17 23:36:35 +0600

    grade calculation

--- gradeCal.go
+++ gradeCal.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func getGradeNumber() int {
 	var a int
-	print("Enter Number: ")
+	print("Enter Your Number: ")
 	fmt.Scan(&a)
+
+	if(a < 0 || a > 100){
+		println("Please enter a valid number between 0 to 100")
+		return getGradeNumber()
+	}
+	
 	return a
 }
 
@@ -17,6 +23,17 @@ func gradeCal(){
 		println("Grade: A+")
 	case number >= 80 && number < 90:
 		println("Grade: A")
+	case number >= 70 && number < 80:
+		println("Grade: B")
+	case number >= 60 && number < 70:
+		println("Grade: C")
+	case number >= 50 && number < 60:
+		println("Grade: D")
+	case number >= 0 && number < 50:
+		println("Grade: F")
+	default:
+		println("Invalid Number!")
+	}
 
 
 }
