4aff186bb7cde572639699556ff15b1c23077542
Prohor722 <prohor.b722@gmail.com>
2025-12-18 23:57:06 +0600

    bmi calculation

--- bmiCal.go
+++ bmiCal.go
@@ -2,5 +2,7 @@ package main
 
 func bmiCal() {
 	// BMI = weight (kg) / height^2 (m^2)
-	var weight, height, bmi float64
+	var weight, height, bmi float64;
+	print("Enter your weight in kg: ")
+	fmt.Scan(&weight)
 }
