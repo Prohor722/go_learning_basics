d718d4de49574fb8ec672d06bab89d81a3277386
Prohor722 <prohor.b722@gmail.com>
2025-12-19 00:13:24 +0600

    bmi calculation

--- bmiCal.go
+++ bmiCal.go
@@ -32,5 +32,14 @@ func bmiCal() {
 	bmi = weight / (height * height)
 
 	println("Your BMI is:", bmi)
-
+	if bmi < 18.5 {
+		println("You are underweight.")
+	} else if bmi >= 18.5 && bmi < 24.9 {
+		println("You have a normal weight.")
+	} else if bmi >= 25 && bmi < 29.9 {
+		println("You are overweight.")
+	}else {
+		println("You are obese.")
+	}
+	
 }
