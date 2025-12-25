6315a08d1075229cc2b864a78157c3d0df41cec2
Prohor722 <prohor.b722@gmail.com>
2025-12-25 23:58:27 +0600

    Arithmetic Progression math formula

--- formula.go
+++ formula.go
@@ -10,3 +10,7 @@ func sequenceFormula() {
 	result := n * (n + 1) / 2
 	println("The sum of the first", n, "natural numbers is:", result)
 }
+
+func arithmeticProgression(a, d, n int) int {
+	return n * (2*a + (n-1)*d) / 2
+}
