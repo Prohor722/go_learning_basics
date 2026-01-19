41f1232dab29b1afd2832c4d132c2ea43404deee
Prohor722 <prohor.b722@gmail.com>
2026-01-19 22:07:44 +0600

    Print All Primes Between 1–N program

--- allPrimesToN.go
+++ allPrimesToN.go
@@ -2,6 +2,11 @@ package main
 
 func allPrimesToN(n int) []int {
 	primes := []int{}
+
 	for i := 2; i <= n; i++ {
 		isPrime := true
-		
+		for j := 2; j*j <= i; j++ {
+			if i%j == 0 {
+				isPrime = false
+				break
+			}
