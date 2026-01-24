84a5928d580079c506d19758306988fcdf1fc0ed
Prohor722 <prohor.b722@gmail.com>
2026-01-24 23:51:18 +0600

    Factorial using Recursion program

--- factorialUsingRecursion.go
+++ factorialUsingRecursion.go
@@ -1,5 +1,11 @@
 package main
 
 func factorialUsingRecursion(){
-	
+	// function to calculate factorial using recursion
+	var factorial func(n int) int
+	factorial = func(n int) int {
+		if n == 0 {
+			return 1
+		}
+		
 }
