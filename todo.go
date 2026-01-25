05a3d67b138d48472e408cc9e646a8306991363b
Prohor722 <prohor.b722@gmail.com>
2026-01-26 03:11:03 +0600

    todo program

--- todo.go
+++ todo.go
@@ -3,6 +3,16 @@ package main
 import "fmt"
 
 func todo(){
+	list := []string{
+		"Build a Todo List Application",
+		"Implement User Authentication",
+		"Add Task Prioritization",
+	}
 	fmt.Println("Welcome to out Todo List Application!!")
+	i := 1
+	for _, item := range list {
+		fmt.Printf("%d. %s\n", i, item)
+		i++
+	}
 
 }
