c69c5c7cf8f4351981005270ea29656b5032cd22
Prohor722 <prohor.b722@gmail.com>
2026-01-26 04:02:39 +0600

    todo program

--- todo.go
+++ todo.go
@@ -1,13 +1,14 @@
 package main
 
 import "fmt"
-
-func readAllTodo(){
-	list := []string{
+var list = []string{
 		"Build a Todo List Application",
 		"Implement User Authentication",
 		"Add Task Prioritization",
 	}
+
+func readAllTodo(){
+	
 	i := 1
 	for _, item := range list {
 		fmt.Printf("%d. %s\n", i, item)
@@ -15,8 +16,12 @@ func readAllTodo(){
 	}
 }
 
+func addTask(task string){
+	list = append(list, task)
+	fmt.Println("Task added:", task)
+}
+
 func todo(){
-	
 	fmt.Println("Welcome to out Todo List Application!!")
 	readAllTodo()
 
