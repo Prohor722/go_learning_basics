e8062b1baeba1a86c9b1f53c1e09fbd7d11e5ff6
Prohor722 <prohor.b722@gmail.com>
2026-02-01 23:10:58 +0600

    Phone Book App

--- phoneBookApp.go
+++ phoneBookApp.go
@@ -91,6 +91,8 @@ func deleteContact(index int) {
 	fmt.Printf("Deleting contact: %s\n", contacts[index].Name)
 	fmt.Println("Are you sure?")
 	var confirmation string
+	fmt.Print("Type 'yes' to confirm: ")
+	fmt.Scanln(&confirmation)
 	contacts = append(contacts[:index], contacts[index+1:]...)
 	fmt.Println("Contact deleted successfully.")
 }
