a52497208acd4d1f919d957b6497286f4b180ac8
Prohor722 <prohor.b722@gmail.com>
2026-02-02 23:29:48 +0600

    lottery App

--- lottery.go
+++ lottery.go
@@ -14,3 +14,6 @@ func drawLottery(winningNumbers []int, userNumbers []int) {
 	if len(matchingNumbers) == 0 {
 		fmt.Println("No matching numbers. Better luck next time!")
 	}
+	if len(matchingNumbers) > 0 {
+		fmt.Printf("Congratulations! You have %d matching numbers: %v\n", len(matchingNumbers), matchingNumbers)
+	}
