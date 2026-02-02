b38a5c07653775b9276faa9a33b99cc1ef64093c
Prohor722 <prohor.b722@gmail.com>
2026-02-02 23:29:22 +0600

    lottery App

--- lottery.go
+++ lottery.go
@@ -4,3 +4,6 @@ import "fmt"
 
 func drawLottery(winningNumbers []int, userNumbers []int) {
 	matchingNumbers := []int{}
+	for _, userNum := range userNumbers {
+		for _, winNum := range winningNumbers {
+			if userNum == winNum {
