0c83d7c36984022c21f591d421e184abf8c1bc3a
Prohor722 <prohor.b722@gmail.com>
2026-02-03 22:28:03 +0600

    lottery App

--- lottery.go
+++ lottery.go
@@ -4,10 +4,10 @@ import "fmt"
 
 type Tickets struct {
 	Numbers []int
-	Owner string
+	Name string
+	Phone string
 }
-
-tickets := []Tickets{}
+var tickets []*Tickets
 var lastSoldTicketIndex int = -1
 
 func drawLottery(winningNumbers []int, userNumbers []int, contact *Contact) {
@@ -17,31 +17,36 @@ func drawLottery(winningNumbers []int, userNumbers []int, contact *Contact) {
 func generateTickets(){
 	fmt.Println("Generating lottery tickets...")
 	for i := 0; i < 5; i++ {
-		ticket := Tickets{Numbers: []int{i, i + 1, i + 2, i + 3, i + 4}, Owner: ""}
-		tickets = append(tickets, ticket)
+		ticket := Tickets{Numbers: []int{i, i + 1, i + 2, i + 3, i + 4}, Name: "", Phone: ""}
+		tickets = append(tickets, &ticket)
 	}
 }
 
-func getTicket(numberOfTickets int) *Tickets {
-	if( lastSoldTicketIndex+1 >= len(tickets) ) {
-		return nil
+func getTicket(numberOfTickets int, name string, phone string) *Tickets {
+
+	for i := 0; i < numberOfTickets; i++ {
+		if( lastSoldTicketIndex+1 >= len(tickets) ) {
+			return nil
+		}
+		lastSoldTicketIndex++
+		tickets[lastSoldTicketIndex].Name = name
+		tickets[lastSoldTicketIndex].Phone = phone
 	}
-	lastSoldTicketIndex += numberOfTickets
-	return &tickets[lastSoldTicketIndex]
+	return tickets[lastSoldTicketIndex]
 }
 
 func buyTickets(){
 	var buyTickets int
 	var name string
 	var phone string
-	fmt.Print("Enter the name of the ticket buyer:")
+	fmt.Print("\nEnter the name of the ticket buyer:")
 	fmt.Scan(&name)
 	fmt.Print("\nEnter the phone number of the ticket buyer:")
 	fmt.Scan(&phone)
 	fmt.Print("\nHow many tickets would you like to buy?")
 	fmt.Scan(&buyTickets)
 
-	if( ticket := getTicket(buyTickets); ticket != nil ) {
+	if( getTicket(buyTickets,name,phone) != nil ) {
 		fmt.Printf("You have successfully bought ticket with numbers: %v\n", ticket.Numbers)
 	}else{
 		fmt.Println("Sorry, not enough tickets available.")
