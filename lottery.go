6f3ec299feef708ca74763e5a53633727b8054cc
Prohor722 <prohor.b722@gmail.com>
2026-02-03 22:03:56 +0600

    lottery App

--- lottery.go
+++ lottery.go
@@ -14,5 +14,8 @@ func drawLottery(winningNumbers []int, userNumbers []int, contact *Contact) {
 
 func generateTickets(){
 	fmt.Println("Generating lottery tickets...")
-
+	for i := 0; i < 5; i++ {
+		ticket := Tickets{Numbers: []int{i, i + 1, i + 2, i + 3, i + 4}}
+		tickets = append(tickets, ticket)
+	}
 }
