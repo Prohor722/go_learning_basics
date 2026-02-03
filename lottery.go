e50e4f015e60da088a2d1b55201d51d7eae9c579
Prohor722 <prohor.b722@gmail.com>
2026-02-03 22:03:15 +0600

    lottery App

--- lottery.go
+++ lottery.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+type Tickets struct {
+	Numbers []int
+}
+
+tickets := []Tickets{}
+
 func drawLottery(winningNumbers []int, userNumbers []int, contact *Contact) {
 
 }
 
 func generateTickets(){
-	
+	fmt.Println("Generating lottery tickets...")
+
 }
