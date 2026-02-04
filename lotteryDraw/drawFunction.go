ba616e4dad8c7844cc5c18c8624dd146b6006bf8
Prohor722 <prohor.b722@gmail.com>
2026-02-04 07:39:14 +0600

    lottery App

--- lotteryDraw/drawFunction.go
+++ lotteryDraw/drawFunction.go
@@ -21,6 +21,6 @@ func drawLottery() {
 	if( winningTicketIndex < 1 ) {
 		fmt.Println("Not enough tickets sold to draw a winner.")
 	}else{
-		fmt.Printf("The winning ticket number is: %s\n", tickets[winningTicketIndex-1].Number)
+		fmt.Printf("The winning ticket number is: %s\n Winner Name: %s & Phone: %s\n", tickets[winningTicketIndex-1].Number, tickets[winningTicketIndex-1].Name, tickets[winningTicketIndex-1].Phone)
 	}
 }
