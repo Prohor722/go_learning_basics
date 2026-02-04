b1cf6c260e5c6eeb18cf0ff5dd26546c7ee87ea0
Prohor722 <prohor.b722@gmail.com>
2026-02-04 07:16:20 +0600

    lottery App

--- lottery.go
+++ lottery.go
@@ -1,17 +1,1 @@
 package main
-
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-type Tickets struct {
-	Number string
-	Name string
-	Phone string
-}
-var tickets []*Tickets
-var lastSoldTicketIndex int = -1
-var numberOfTickets int
-
--- lotteryDraw/main.go
+++ lotteryDraw/main.go
@@ -1,5 +1,16 @@
 package main
 
+type Tickets struct {
+	Number string
+	Name string
+	Phone string
+}
+var tickets []*Tickets
+var lastSoldTicketIndex int = -1
+var numberOfTickets int
+
+
+
 func main(){
 	drawLottery()
 }
