e18f4b0e3ac6a522542dadda79102de9aa9d8c57
Prohor722 <prohor.b722@gmail.com>
2026-02-04 06:14:23 +0600

    lottery App

--- lottery.go
+++ lottery.go
@@ -19,6 +19,13 @@ func drawLottery() {
 
 	// fmt.Printf("Winning Numbers: %v\n", winningNumbers)
 }
+func winningNumbers() []int {
+	// For simplicity, let's assume the winning numbers are the first 5 numbers
+	if len(tickets) < 5 {
+		return []int{}
+	}
+	return tickets[0].Numbers
+}
 
 func generateTickets(numberOfTickets int) {
 	fmt.Println("Generating lottery tickets...")
