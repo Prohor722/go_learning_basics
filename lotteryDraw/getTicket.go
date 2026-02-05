6730ea5644250ecfc66102a6c0e6954a1628c0bc
Prohor722 <prohor.b722@gmail.com>
2026-02-05 06:57:45 +0600

    lottery App

--- lotteryDraw/getTicket.go
+++ lotteryDraw/getTicket.go
@@ -6,9 +6,12 @@ func getTicket(buyTickets int, name string, phone string) bool {
 
 	for i := 0; i < buyTickets; i++ {
 		if lastSoldTicketIndex >= len(tickets)-1 || tickets[lastSoldTicketIndex+1].Name != "" || lastSoldTicketIndex+1+buyTickets > totalLotteryTickets {
-			fmt.Printf("lastSoldTicketIndex >= len(tickets)-1: %v",lastSoldTicketIndex >= len(tickets)-1)
-			fmt.Printf("tickets[lastSoldTicketIndex+1].Name != \"\": %v", tickets[lastSoldTicketIndex+1].Name != "")
-			fmt.Printf("lastSoldTicketIndex+1+buyTickets > totalLotteryTickets: %v", lastSoldTicketIndex+1+buyTickets > totalLotteryTickets)
+			// fmt.Printf("lastSoldTicketIndex >= len(tickets)-1: %v",lastSoldTicketIndex >= len(tickets)-1)
+			// fmt.Printf("tickets[lastSoldTicketIndex+1].Name != \"\": %v", tickets[lastSoldTicketIndex+1].Name != "")
+			fmt.Printf("lastSoldTicketIndex+1+buyTickets > totalLotteryTickets: %v\n", lastSoldTicketIndex+1+buyTickets > totalLotteryTickets)
+			fmt.Printf("lastSoldTicketIndex: %d\n",lastSoldTicketIndex)
+			fmt.Printf("buyTickets: %d\n", buyTickets)
+			fmt.Printf("totalLotteryTickets: %d\n", totalLotteryTickets)
 			return false
 		}
 		lastSoldTicketIndex++
