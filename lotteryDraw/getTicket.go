6fe0ca7f9defc113c5fc39f7f71c8e8d94566f45
Prohor722 <prohor.b722@gmail.com>
2026-02-05 07:03:33 +0600

    lottery App

--- lotteryDraw/getTicket.go
+++ lotteryDraw/getTicket.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func getTicket(buyTickets int, name string, phone string) bool {
+	var logic1 bool = lastSoldTicketIndex >= len(tickets)-1
+	var logic2 bool = tickets[lastSoldTicketIndex+1].Name != ""
+	var logic3 bool = lastSoldTicketIndex+buyTickets > totalLotteryTickets
 
 	for i := 0; i < buyTickets; i++ {
-		if lastSoldTicketIndex >= len(tickets)-1 || tickets[lastSoldTicketIndex+1].Name != "" || lastSoldTicketIndex+buyTickets > totalLotteryTickets {
+		if logic1 || logic2 || logic3 {
 			// fmt.Printf("lastSoldTicketIndex >= len(tickets)-1: %v",lastSoldTicketIndex >= len(tickets)-1)
 			// fmt.Printf("tickets[lastSoldTicketIndex+1].Name != \"\": %v", tickets[lastSoldTicketIndex+1].Name != "")
-			fmt.Printf("lastSoldTicketIndex+1+buyTickets > totalLotteryTickets: %v\n", lastSoldTicketIndex+1+buyTickets > totalLotteryTickets)
+			fmt.Printf("lastSoldTicketIndex+buyTickets > totalLotteryTickets: %v\n", logic3)
 			fmt.Printf("lastSoldTicketIndex: %d\n",lastSoldTicketIndex)
 			fmt.Printf("buyTickets: %d\n", buyTickets)
 			fmt.Printf("totalLotteryTickets: %d\n", totalLotteryTickets)
