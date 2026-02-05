5e8ae64b0faed78026528c20f1ec0eda7d939375
Prohor722 <prohor.b722@gmail.com>
2026-02-05 06:53:10 +0600

    lottery App

--- lotteryDraw/getTicket.go
+++ lotteryDraw/getTicket.go
@@ -1,9 +1,14 @@
 package main
 
+import "fmt"
+
 func getTicket(buyTickets int, name string, phone string) bool {
 
 	for i := 0; i < buyTickets; i++ {
-		if( lastSoldTicketIndex >= len(tickets)-1 || tickets[lastSoldTicketIndex+1].Name != "" || lastSoldTicketIndex+1+buyTickets > totalLotteryTickets ){ 
+		if lastSoldTicketIndex >= len(tickets)-1 || tickets[lastSoldTicketIndex+1].Name != "" || lastSoldTicketIndex+1+buyTickets > totalLotteryTickets {
+			fmt.Printf("lastSoldTicketIndex >= len(tickets)-1: %v",lastSoldTicketIndex >= len(tickets)-1)
+			fmt.Printf("tickets[lastSoldTicketIndex+1].Name != \"\": %v", tickets[lastSoldTicketIndex+1].Name != "")
+			fmt.Printf("lastSoldTicketIndex+1+buyTickets > totalLotteryTickets: %v", lastSoldTicketIndex+1+buyTickets > totalLotteryTickets)
 			return false
 		}
 		lastSoldTicketIndex++
