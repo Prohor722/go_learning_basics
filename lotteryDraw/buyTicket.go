6d4d56f0a7aa96a0b24c33da8773a319069159b4
Prohor722 <prohor.b722@gmail.com>
2026-02-06 23:43:01 +0600

    lottery App

--- lotteryDraw/buyTicket.go
+++ lotteryDraw/buyTicket.go
@@ -9,15 +9,9 @@ func buyTickets() bool {
 	var name string
 	var phone string
 	fmt.Print("\nEnter the name of the ticket buyer:")
-	fmt.Scan(&name)
-	if !validateName(name) {
-		return false
-	}	
+	name = getName()
 	fmt.Print("\nEnter the phone number of the ticket buyer:")
-	fmt.Scan(&phone)
-	if !validatePhone(phone) {
-		return false
-	}
+	phone = getPhone()
 	fmt.Print("\nHow many tickets would you like to buy:")
 	fmt.Scan(&buyTickets)
 	if !validNumber(buyTickets) {
