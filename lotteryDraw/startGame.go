2cc673de560dbb3c38cfb2adacad9d57ea6260ac
Prohor722 <prohor.b722@gmail.com>
2026-02-06 23:30:16 +0600

    lottery App

--- lotteryDraw/startGame.go
+++ lotteryDraw/startGame.go
@@ -6,6 +6,10 @@ func startGame() {
 	fmt.Print("Welcome To the Lottery Game !!\n")
 	fmt.Printf("Number of Tickets to play the Game:")
 	fmt.Scan(&totalLotteryTickets)
+
+	fmt.Printf("Select a option:\n")
+	fmt.Printf("1. Buy Tickets\n")
+	fmt.Printf("2. Draw Lottery\n")
 	
 	if(!validNumber(totalLotteryTickets) || totalLotteryTickets < 2){
 		fmt.Println("Invalid number of tickets. Exiting the game. (Min:2)")
@@ -14,6 +18,11 @@ func startGame() {
 	if(!generateTickets(totalLotteryTickets)) {
 		fmt.Println("Failed to generate tickets. Restarting the game.")
 		startGame()
+		return
+	}
+	if(!buyTickets()){
+		fmt.Println("Ticket purchase failed. Restarting ticket purchase.")
+		drawLottery()
 		return
 	}
 	fmt.Println()
