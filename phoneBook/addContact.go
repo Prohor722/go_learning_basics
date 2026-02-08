c080b9f7e3b8580f72df7edea0c98043c99c9f67
Prohor722 <prohor.b722@gmail.com>
2026-02-08 23:00:50 +0600

    Phonebook App

--- phoneBook/addContact.go
+++ phoneBook/addContact.go
@@ -16,8 +16,6 @@ func addContact() []*Contact {
 	if !contactExists(name, phone) {
 		contacts = append(contacts, &Contact{Name: name, Phone: phone, Email: email})
 		fmt.Printf("Contact added! Name: %s, Phone: %s, Email: %s\n", name, phone, email)
-		fmt.Println("Current Contacts:")
-		printContacts()
 	}
 	return contacts
 }
