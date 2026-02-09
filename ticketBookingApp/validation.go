4b2fcc5958e04c401b67f9c1a9caa10a3f3cef00
Prohor722 <prohor.b722@gmail.com>
2026-02-09 23:56:29 +0600

    TicketBooking App

---
+++ ticketBookingApp/validation.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"strings"
+)
+
+func validation(validationType string, value interface{}) bool {
+	switch validationType {
+	case "name":
+		name, ok := value.(string)
+		if !ok || len(name) < 2 {
+			return false
+		}
+		return true
+	case "age":
+		age, ok := value.(int)
+		if !ok || age < 0 || age > 120 {
+			return false
+		}
+		return true
+	case "email":
+		email, ok := value.(string)
+		if !ok || len(email) < 5 || !strings.Contains(email, "@") {
+			return false
+		}
+		return true
+	default:
+		return false
+	}
+}
