70ad075b6ab7233311d976f2e719ab3d779b1199
Prohor722 <prohor.b722@gmail.com>
2026-02-10 00:27:54 +0600

    TicketBooking App

--- ticketBookingApp/main.go
+++ ticketBookingApp/main.go
@@ -8,16 +8,19 @@ type Movie struct {
 
 var movies = []Movie{
         {
+			id:			   1,
             Name:             "The Matrix",
             Price:            12.99,
             AvailableTickets: 50,
         },
         {
+			id:			   2,
             Name:             "Inception",
             Price:            14.99,
             AvailableTickets: 30,
         },
         {
+			id:			   3,
             Name:             "Interstellar",
             Price:            15.99,
             AvailableTickets: 25,
