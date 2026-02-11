b338f40395d811635adf57aa052716749933254b
Prohor722 <prohor.b722@gmail.com>
2026-02-11 23:35:37 +0600

    Quiz Game App

--- quizGame/main.go
+++ quizGame/main.go
@@ -66,20 +66,6 @@ var quiz = Quiz{
 		},
 }
 
-var questionsA = []string{
-		"What is the capital of France?",
-		"What is 2 + 2?",
-		"What is the largest planet in our solar system?",
-	}
-
-var optionsA = [][]string{
-		{"A) Berlin", "B) Madrid", "C) Paris", "D) Rome"},
-		{"A) 3", "B) 4", "C) 5", "D) 6"},
-		{"A) Earth", "B) Jupiter", "C) Mars", "D) Saturn"},
-	}
-
-var correctAnswersA = []string{"C", "B", "B"}
-
 func main() {
 	quizGame()
 }
