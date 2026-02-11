2c0187f92b8b59551b96bc3ecef194ef4c57f327
Prohor722 <prohor.b722@gmail.com>
2026-02-11 23:12:51 +0600

    Quiz Game App

--- quizGame/validateFunctions.go
+++ quizGame/validateFunctions.go
@@ -1,1 +1,8 @@
 package main
+
+func validateName (name string) bool {
+	name = strings.TrimSpace(name)
+	if len(name) == 0 {
+		println("Name cannot be empty. Please enter a valid name.")
+		return false
+	}
