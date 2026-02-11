6c1528a89d0d397eab2fe84fefa5aae6d21dbd2c
Prohor722 <prohor.b722@gmail.com>
2026-02-11 23:39:37 +0600

    Quiz Game App

--- quizGame/quizGame.go
+++ quizGame/quizGame.go
@@ -10,11 +10,14 @@ func quizGame() {
 
 	println("Hello", name, "age", age, "! Let's start the quiz.")
 
-	if(age < 18) {
-		println("Note: Some questions may be more challenging for younger players.")
+	if(age < 12) {
+		getUserAnswer(quiz.A_questions)
+	} else if(age < 15) {
+		getUserAnswer(quiz.B_questions)
+	} else {
+		getUserAnswer(quiz.C_questions)
 	}
 
-	getUserAnswer(questionsA, optionsA, correctAnswersA)
 	
 	println("\nThank you for playing, ", name, "!")
 
