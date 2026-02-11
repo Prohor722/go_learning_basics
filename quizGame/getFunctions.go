f6d6f4b5299ef5226f62b4b45f208c9e7c4aeeef
Prohor722 <prohor.b722@gmail.com>
2026-02-11 23:17:51 +0600

    Quiz Game App

--- quizGame/getFunctions.go
+++ quizGame/getFunctions.go
@@ -1,12 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func getName() string {
 	var name string
-	print("Enter your name: ")
+	print("\nEnter your name: ")
 	fmt.Scanln(&name)
-	name = strings.TrimSpace(name)
 	if !validateName(name) {
 		return getName()
 	}
 	return name
+}
+
+func getAge(name string) uint {
+	var age uint = 0
+	print("Enter your age: ")
+	fmt.Scanln(&age)
+	age = uint(math.Abs(float64(age)))
+	if !validateAge(age) {
+		return getAge(name)
+	}
+	return age
 }
