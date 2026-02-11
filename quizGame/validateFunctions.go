d5d04daa73ecbdaf6b0ff8db965d04b397317eaa
Prohor722 <prohor.b722@gmail.com>
2026-02-11 23:13:42 +0600

    Quiz Game App

--- quizGame/validateFunctions.go
+++ quizGame/validateFunctions.go
@@ -6,8 +6,8 @@ import (
 
 func validateName (name string) bool {
 	name = strings.TrimSpace(name)
-	if len(name) == 0 {
-		println("Name cannot be empty. Please enter a valid name.")
+	if len(name) < 2 {
+		println("Please enter a valid name (at least 2 characters).")
 		return false
 	}
 	return true
