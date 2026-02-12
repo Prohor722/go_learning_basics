06ff59081beadcf61038dbae84777c4343ec65c5
Prohor722 <prohor.b722@gmail.com>
2026-02-12 23:12:03 +0600

    Pointer Example

--- pointerExample.go
+++ pointerExample.go
@@ -8,6 +8,10 @@ type Person struct {
 	address string
 }
 
+func (p *Person) incresseAge (incresseBy int) {
+	p.age += incresseBy
+}
+
 func pointerExample() {
 	person1 := Person{name: "Alice", age: 30, address: "123 Main St"}
 	fmt.Printf("Before modification: %+v\n", person1)
