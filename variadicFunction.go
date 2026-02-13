0bcc46e1c87efb35e08f2529fe9b1603ca6b6225
Prohor722 <prohor.b722@gmail.com>
2026-02-13 12:58:27 +0600

    Variadic Function Example

--- variadicFunction.go
+++ variadicFunction.go
@@ -6,6 +6,11 @@ func variadicFunction(){
 	fmt.Println("Enter how many numbers you want to sum:")
 	var n int
 	fmt.Scan(&n)
+	fmt.Println("Enter the numbers:")
+	nums := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&nums[i])
+	}
 
 	println(sumOfNumbers(1, 2, 3, 4, 5)) // Output: 15
 }
