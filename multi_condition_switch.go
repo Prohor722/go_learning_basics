37c6fd969f04b89da575de48879316fae978a638
Prohor722 <prohor.b722@gmail.com>
2026-02-16 23:25:59 +0600

    multiple condition switch

--- multi_condition_switch.go
+++ multi_condition_switch.go
@@ -3,6 +3,10 @@ package main
 import "time"
 
 func multiConditionSwitch() {
-	switch time.Now {
+	switch time.Now().Weekday() {
+	case time.Friday, time.Saturday:
+		println("It's a weekend")
+	default:
+		println("It's a weekday")
 	}
 }
