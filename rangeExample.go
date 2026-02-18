e621d3262c56deef7a381c5d14865c8125e4ec19
Prohor722 <prohor.b722@gmail.com>
2026-02-18 23:17:38 +0600

    range examples

--- rangeExample.go
+++ rangeExample.go
@@ -1,3 +1,9 @@
 package main
 
-func rangeExample() {}
+//range is used to iterate over elements in a variety of data structures, such as arrays, slices, maps, and strings. It provides a convenient way to loop through these data structures without having to manage the index or key manually.
+func rangeExample() {
+	nums := []int{1, 2, 3, 4, 5}
+	for index, value := range nums {
+		println("Index:", index, "Value:", value)
+	}
+}
