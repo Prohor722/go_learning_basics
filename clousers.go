39a4fefdb37491e1861261f26db23a950383ce9c
Prohor722 <prohor.b722@gmail.com>
2026-02-19 23:23:05 +0600

    clousers examples

--- clousers.go
+++ clousers.go
@@ -1,14 +1,18 @@
 package main
 
+import "fmt"
+
 func counter() func() int {
 	var count int = 0
-	
 	return func() int {
 		count++
 		return count
 	}
 }
 
-func clousersExample(){
+func clousersExample() {
+	increment := counter()
+
+	fmt.Println(increment())
 
 }
