8abca5f7eb5e5766451d315c6b7c7c51def067c5
Prohor722 <prohor.b722@gmail.com>
2026-02-20 22:35:02 +0600

    interface examples

--- interface.go
+++ interface.go
@@ -1,5 +1,22 @@
 package main
 
-func interfaceExample(){
-	
+import "fmt"
+
+type payment struct {
+}
+
+func (p payment) makePayment(amount float32) {
+	razorpayPaymentGW := razorpay{}
+	razorpayPaymentGW.pay(amount)
+}
+
+type razorpay struct{}
+
+func (r razorpay) pay(amount float32) {
+	fmt.Println("making payment using razorpay", amount)
+}
+
+func interfaceExample() {
+	newPayment := payment{}
+	newPayment.makePayment(100)
 }
