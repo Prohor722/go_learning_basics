d6067237f6ef34ad5a3948b367cccfc1324b0ce7
Prohor722 <prohor.b722@gmail.com>
2026-02-21 04:00:10 +0600

    enum examples

--- enums.go
+++ enums.go
@@ -11,8 +11,23 @@ const (
 	Delivered
 )
 
+func getStatus(status OrderStatus) string{
+	switch status{
+	case 0:
+		return "Received"
+	case 1:
+		return "Confirmed"
+	case 2:
+		return "Prepared"
+	case 3:
+		return "Delivered"
+	default:
+		return "ERROR!!"
+	}
+}
+
 func changeOrderStatus(status OrderStatus) {
-	fmt.Println("changing order status to: ",status)
+	fmt.Println("changing order status to: ",getStatus(status))
 }
 
 func enumExamples() {
