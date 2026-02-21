cd3cb347bd22b7c40cc849a5ba3a4a49f3f56a6a
Prohor722 <prohor.b722@gmail.com>
2026-02-21 07:32:19 +0600

    goroutine examples

--- goRoutines/main.go
+++ goRoutines/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"time"
+)
+
+func task(id int) {
+	fmt.Println("Doing task:", id)
+}
+
+func main() {
+	for i:=range 11 {
+		// go task(i)
+		go func(i int){
+			fmt.Println(i)
+		}(i)
+	}
+
+	time.Sleep(time.Microsecond*200)
 }
