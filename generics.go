8f793eb381e4768d02f11e4dd5e7a8e96cea3033
Prohor722 <prohor.b722@gmail.com>
2026-02-21 07:11:07 +0600

    generic examples

--- generics.go
+++ generics.go
@@ -9,7 +9,25 @@ func printSlice[T int | string](items [] T) {
 	}
 }
 
+type stack[T int|string] struct{
+	elements [] T
+}
+
 func genericExample() {
+	myStack := stack[string]{
+		elements: []string{
+			"Hello",
+			"Brother",
+		},
+	}
+
+	// myStack.elements = []int{	//only can reassign the same type data
+	// 	1,
+	// 	2,
+	// }
+
+	fmt.Println(myStack)
+
 	nums := []int{1,2,3,4,5}
 	names := []string{"golang","ts","js"}
 	printSlice(names)
