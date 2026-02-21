2a50275ee9fb01aff4d1dff65e0655faf0355c2b
Prohor722 <prohor.b722@gmail.com>
2026-02-21 22:33:35 +0600

    file examples

--- file/files.go
+++ file/files.go
@@ -1,5 +1,21 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("file.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fileInfo, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("File Name",fileInfo.Name())
 }
