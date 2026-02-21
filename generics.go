dfc039921e7ff6491aae5cdb8e8c02a38817add0
Prohor722 <prohor.b722@gmail.com>
2026-02-21 07:00:03 +0600

    generic examples

--- generics.go
+++ generics.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 
-func printSlice[T any](items [] T) {
+func printSlice[T int | string](items [] T) {
 	for _, item := range items {
 		fmt.Println(item)
 	}
 }
 
 func genericExample() {
-	// nums := []int{1,2,3,4,5}
+	nums := []int{1,2,3,4,5}
 	names := []string{"golang","ts","js"}
 	printSlice(names)
+	printSlice(nums)
 }
