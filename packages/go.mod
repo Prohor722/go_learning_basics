cc29e73e01e4468c4dfee7b661e77c724800a75e
Prohor722 <prohor.b722@gmail.com>
2026-02-22 23:44:14 +0600

    package examples

---
+++ packages/go.mod
@@ -0,0 +1,3 @@
+module github.com/Prohor722/go_learning_basics
+
+go 1.25.4
--- packages/main.go
+++ packages/main.go
@@ -1,5 +1,8 @@
 package main
 
+// go mod init github.com/Prohor722/go_learning_basics	//cmd
+//naming by the github project url is convension not mendatory
+
 func main(){
-	
+
 }
