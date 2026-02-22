1afbd29830a2cd3437d2303231c7445152191b4a
Prohor722 <prohor.b722@gmail.com>
2026-02-22 23:54:45 +0600

    package examples

---
+++ packages/auth/credentials.go
@@ -0,0 +1,7 @@
+package auth
+
+import "fmt"
+
+func LoginWithCredentials(username string, pass string) {
+	fmt.Println("login user using",username,pass)
+}
--- packages/main.go
+++ packages/main.go
@@ -1,8 +1,10 @@
 package main
 
+import "github.com/Prohor722/go_learning_basics/auth"
+
 // go mod init github.com/Prohor722/go_learning_basics	//cmd
 //naming by the github project url is convension not mendatory
 
 func main(){
-
+	auth.LoginWithCredentials("Prohor","1234")
 }
